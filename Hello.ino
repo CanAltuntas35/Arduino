077c0c0d822d1c523411035baa2de34c8f072f4c
CanAltuntas35 <56817818+CanAltuntas35@users.noreply.github.com>
2023-06-21 15:47:05 +0300

    Add files via upload

---
+++ Hello.ino
@@ -0,0 +1,213 @@
+
+
+#include <Servo.h>
+
+
+int servoPin = 8;
+int PEN_DOWN = 20;         // servo angle
+int PEN_UP = 80;   
+Servo penServo;
+
+float wheel_dia=63;    
+float wheel_base=109;      
+int steps_rev=128;          // 128.16x
+
+
+
+int L_stepper_pins[] = {12, 10, 9, 11};
+int R_stepper_pins[] = {4, 6, 7, 5};
+
+int fwd_mask[][4] =  {{1, 0, 1, 0},
+                      {0, 1, 1, 0},
+                      {0, 1, 0, 1},
+                      {1, 0, 0, 1}};
+
+int rev_mask[][4] =  {{1, 0, 0, 1},
+                      {0, 1, 0, 1},
+                      {0, 1, 1, 0},
+                      {1, 0, 1, 0}};
+
+
+void setup() {
+  randomSeed(analogRead(1)); 
+  Serial.begin(9600);
+  for(int pin=0; pin<4; pin++){
+    pinMode(L_stepper_pins[pin], OUTPUT);
+    digitalWrite(L_stepper_pins[pin], LOW);
+    pinMode(R_stepper_pins[pin], OUTPUT);
+    digitalWrite(R_stepper_pins[pin], LOW);
+  }
+  penServo.attach(servoPin);
+  Serial.println("setup");
+  
+
+  
+  delay(1000);
+}
+
+
+void loop(){ 
+    
+pendown();
+  forward(200);
+  backward(100);
+  left(373);
+  forward(90);         //H
+  left(370);
+  forward(100);
+  backward(200);
+  penup();
+  right(370);
+  forward(60);
+
+  pendown();
+  forward(110);
+  backward(108);
+  left(370);
+  forward(100);     //E
+  right(370);
+  forward(80);
+  backward(80);
+  left(370);
+  forward(100);
+  right(370);
+  forward(105);
+  penup();
+
+  forward(60);
+  pendown();
+  right(370);
+  forward(200);     // L 1
+  left(370);
+  forward(110);
+  penup();
+  forward(60);
+
+  pendown();
+  forward(110);
+  backward(108);
+  left(370);
+  forward(200);   // L 2
+  penup();
+  right(370);
+  forward(170);
+
+
+  pendown();
+  forward(100);
+  right(270);
+  forward(50);
+  right(100);
+  forward(100);
+  right(100);
+  forward(50);
+  right(270);       //O
+  forward(100);
+  right(270);
+  forward(50);
+  right(100);
+  forward(100);
+  right(150);
+  forward(63);
+  penup();
+  right(270);
+  forward(500);
+
+
+
+
+  
+  done();     
+  while(1);   
+}
+
+
+
+
+void forward(float distance){
+  int steps = step(distance);
+  Serial.println(steps);
+  for(int step=0; step<steps; step++){
+    for(int mask=0; mask<4; mask++){
+      for(int pin=0; pin<4; pin++){
+        digitalWrite(L_stepper_pins[pin], rev_mask[mask][pin]);
+        digitalWrite(R_stepper_pins[pin], fwd_mask[mask][pin]);
+      }
+      delay(2.5);
+    } 
+  }
+}
+
+
+void backward(float distance){
+  int steps = step(distance);
+  for(int step=0; step<steps; step++){
+    for(int mask=0; mask<4; mask++){
+      for(int pin=0; pin<4; pin++){
+        digitalWrite(L_stepper_pins[pin], fwd_mask[mask][pin]);
+        digitalWrite(R_stepper_pins[pin], rev_mask[mask][pin]);
+      }
+      delay(3);
+    } 
+  }
+}
+
+
+void right(float degrees){
+  float rotation = degrees / 360.0;
+  float distance = wheel_base * 3.1412 * rotation;
+  int steps = step(distance);
+  for(int step=0; step<steps; step++){
+    for(int mask=0; mask<4; mask++){
+      for(int pin=0; pin<4; pin++){
+        digitalWrite(R_stepper_pins[pin], rev_mask[mask][pin]);
+        digitalWrite(L_stepper_pins[pin], rev_mask[mask][pin]);
+      }
+      delay(2);
+    } 
+  }   
+}
+
+
+void left(float degrees){
+  float rotation = degrees / 360.0;
+  float distance = wheel_base * 3.1412 * rotation;
+  int steps = step(distance);
+  for(int step=0; step<steps; step++){
+    for(int mask=0; mask<4; mask++){
+      for(int pin=0; pin<4; pin++){
+        digitalWrite(R_stepper_pins[pin], fwd_mask[mask][pin]);
+        digitalWrite(L_stepper_pins[pin], fwd_mask[mask][pin]);
+      }
+      delay(2);
+    } 
+  }   
+}
+
+
+void done(){ 
+  for(int mask=0; mask<4; mask++){
+    for(int pin=0; pin<4; pin++){
+      digitalWrite(R_stepper_pins[pin], LOW);
+      digitalWrite(L_stepper_pins[pin], LOW);
+    }
+    delay(2);
+  }
+}
+
+
+void penup(){
+  delay(250);
+  Serial.println("PEN_UP()");
+  penServo.write(PEN_UP);
+  delay(250);
+}
+
+
+void pendown(){
+  delay(250);  
+  Serial.println("PEN_DOWN()");
+  penServo.write(PEN_DOWN);
+  delay(250);
+}
+
